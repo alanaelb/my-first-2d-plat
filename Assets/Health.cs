e8579e59e4473568505a9b461988ec6cfce9cd13
Alana <alana.elbaghd@studytafensw.edu.au>
2023-08-02 13:57:24 +1000

    health and player manager
    h and pm

---
+++ Assets/Health.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int _health;
+    private int _maxHealth = 100;
+
+    public void Damage(int damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            //_health = 0;
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void Heal(int heal)
+    {
+        _health = Mathf.Min(_maxHealth, _health + heal);
+    }
+
+}
