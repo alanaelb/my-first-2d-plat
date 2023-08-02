3ca285873e4af83d47cfd9bcb7dbe41e2cdc5946
Alana <alana.elbaghd@studytafensw.edu.au>
2023-08-02 15:47:25 +1000

    bullet and gun script

---
+++ Assets/Gun.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject BulletPreFab;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+
+            Instantiate(BulletPreFab);
+        }
+    }
+}
